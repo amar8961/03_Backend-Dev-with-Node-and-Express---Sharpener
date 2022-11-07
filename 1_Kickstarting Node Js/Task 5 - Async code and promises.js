c62fdd4372182bbc129e190cead00a690e80104c
amar8961 <amarkumar8961@gmail.com>
2022-11-07 19:12:11 +0530

    Async Code & Promises -> Task - 5

---
+++ 1_Kickstarting Node Js/Task 5 - Async code and promises.js
@@ -0,0 +1,47 @@
+// Async Code & Promises
+// Async Code
+const fetchData = (callback) => {
+  setTimeout(() => {
+    callback("Done!");
+  }, 1500);
+};
+
+// Async / asynchronous code -> because it does not execute immediately.
+setTimeout(() => {
+  console.log("Timer is Done");
+  fetchData((text) => {
+    console.log(text);
+  });
+}, 2000);
+
+// synchronous code -> because it execute immediately.
+console.log("Hello!");
+console.log("Hi!");
+
+// Promises
+const fetchData2 = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Promise-Done!");
+    }, 1500);
+  });
+  return promise;
+};
+
+// Async / asynchronous code -> because it does not execute immediately.
+setTimeout(() => {
+  console.log("Promise-Timer is Done");
+  fetchData2()
+    .then((text2) => {
+      console.log(text2);
+      return fetchData2();
+      // again return fetchData2() -> because if we have another use like this.
+    })
+    .then((text3) => {
+      console.log(text3);
+    });
+}, 5000); // 5000 millisecond = 5 second
+
+// synchronous code -> because it execute immediately.
+console.log("Promise-Hello!");
+console.log("Promise-Hi!");
