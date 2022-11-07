6948ec69a6d77ae05bab76cb4b5884c914ab8a60
amar8961 <amarkumar8961@gmail.com>
2022-11-07 19:03:36 +0530

    Task Question -> Task 4

--- 1_Kickstarting Node Js/Task 4 - Destructuring.js
+++ 1_Kickstarting Node Js/Task 4 - Destructuring.js
@@ -33,3 +33,24 @@ const [hobb1, hobb2] = hobbies;
 console.log(hobb1, hobb2);
 // in the output we not get square brackets because we're not locking an array here
 // we are logging the individual values which we get via array destructuring.
+
+//  *** Task - Q) What will be the output of the following ? ***
+// 1
+const obj1 = {'key1': 1, "key2": 2, "key3": 1000}
+const { key1, key3 } = { ...obj1 };
+console.log(key1, key3);
+
+// 2
+const arr1 = ['value1', 'value2']
+const [ val1, val2 ] = arr1
+
+console.log(val1)
+console.log(val2)
+
+// 3
+const obj2 = {'key1': 1, "key2": 2, "key3": 1000}
+let { key4, key6}  = obj2
+key4 = 20;
+key6 = 123
+
+console.log(obj2.key1, obj2.key3)
