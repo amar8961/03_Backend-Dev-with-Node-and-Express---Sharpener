99c1cdb44cc82457a217d4eb767a82774ec75bf4
amar8961 <amarkumar8961@gmail.com>
2022-11-09 02:43:19 +0530

    UPDATE -> Task - 9

--- 1_Kickstarting Node Js/Task 9 - routes - Clean Up Code.js
+++ 1_Kickstarting Node Js/Task 9 - routes - Clean Up Code.js
@@ -30,7 +30,7 @@ const requestHandler = ((req, res) => {
     }
     console.log(req.url, req.method, req.headers);
     res.setHeader('Content-Type', 'text/html');
-    res.write('</html>')
+    res.write('<html>')
     res.write('<head><title>My First Page</title></head>')
     res.write('<body><h1>Hello from my Node.js Server !</h1></body>')
     res.write('</html>')
