70e762ee7e8ae63d7d37cbd1f3242b6856bcd16e
amar8961 <amarkumar8961@gmail.com>
2022-11-08 17:31:48 +0530

    Task Question -> Task - 7

--- 1_Kickstarting Node Js/Task 7 - Request & Response.js
+++ 1_Kickstarting Node Js/Task 7 - Request & Response.js
@@ -124,21 +124,103 @@
 // server.listen(3000);
 
 
-//  *** Understanding Event Driven Code Execution ***
+// //  *** Understanding Event Driven Code Execution ***
+// const http = require("http");
+// const fs = require('fs');  // import 'fs' core module
+
+// const server = http.createServer((req, res) => {
+//     const url = req.url;
+//     const method = req.method;
+//     if(url === '/') {
+//         res.write('<html>')
+//         res.write('<head><title>Enter Mesage</title></head>')
+//         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>')
+//         res.write('</html>')
+//         return res.end();
+//     }
+//     if(url === '/message' && method === 'POST'){
+//       const body = [];
+//       req.on('data', (chunk) => {
+//         console.log(chunk)
+//         body.push(chunk);
+//       });
+//       req.on('end', () => {
+//         const parsedBody = Buffer.concat(body).toString()
+//         const message = parsedBody.split('=')[1];
+//         console.log(parsedBody)
+//         fs.writeFileSync('Task 7 - message(Understanding Event Driven Code Execution).text', message)
+//         res.statusCode = 302; 
+//         res.setHeader('Location', '/')
+//         return res.end();   
+//       })
+//     }
+//   console.log(req.url, req.method, req.headers);
+//   res.setHeader('Content-Type', 'text/html');
+//   res.write('<head><title>My First Page</title></head>')
+//   res.write('<body><h1>Hello from my Node.js Server !</h1></body>')
+//   res.write('</html>')
+//   res.end()
+// });
+// server.listen(3000);
+
+
+//  *** Task Question ***
+// // 1)
+// const http = require("http");
+// const fs = require('fs');  // import 'fs' core module
+
+// const server = http.createServer((req, res) => {
+//     const url = req.url;
+//     const method = req.method;
+//     if(url === '/') {
+//         res.write('<html>')
+//         res.write('<head><title>Enter Mesage</title></head>')
+//         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>')
+//         res.write('</html>')
+//         return res.end();
+//     }
+//     if(url === '/message' && method === 'POST'){
+//       const body = [];
+//       req.on('data', (chunk) => {
+//         console.log(chunk)
+//         body.push(chunk);
+//       });
+//       req.on('end', () => {
+//         const parsedBody = Buffer.concat(body).toString()
+//         const message = parsedBody.split('=')[1];
+//         console.log(parsedBody)
+//         fs.writeFileSync('Task 7 - message(Understanding Event Driven Code Execution).text', message)
+//         res.statusCode = 302; 
+//         res.setHeader('Location', '/')
+//         return res.end();   
+//       })
+//     }
+//   console.log(req.url, req.method, req.headers);
+//   res.setHeader('Content-Type', 'text/html');
+//   res.write('<head><title>My First Page</title></head>')
+//   res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+//   res.write('</html>')
+//   res.end()
+// });
+// server.listen(3000);
+
+// 2)
 const http = require("http");
 const fs = require('fs');  // import 'fs' core module
 
 const server = http.createServer((req, res) => {
+  // req = request , res = response
     const url = req.url;
     const method = req.method;
     if(url === '/') {
         res.write('<html>')
         res.write('<head><title>Enter Mesage</title></head>')
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>')
+        res.write('<body><nav><a href="/home">Home</a> <a href="/about">About</a><br><a href="/node">Node</a></nav></body>')
         res.write('</html>')
         return res.end();
     }
-    if(url === '/message' && method === 'POST'){
+    if(url === '/message'){
       const body = [];
       req.on('data', (chunk) => {
         console.log(chunk)
@@ -153,12 +235,31 @@ const server = http.createServer((req, res) => {
         res.setHeader('Location', '/')
         return res.end();   
       })
+    } else if(url === '/home'){
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<head><title>Welcome to About Us page</title></head>')
+      res.write('<body><h1>Welcome home</h1></body>')
+      res.write('</html>')
+      return res.end();  // we have to return to keep server running otherwise server stop working.
+    } else if(url === '/about'){
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<head><title>Welcome to About Us page</title></head>')
+      res.write('<body><h1>Welcome to About Us page</h1></body>')
+      res.write('</html>')
+      return res.end()
+    } else if(url === '/node'){
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<head><title>My First Page</title></head>')
+      res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+      res.write('</html>')
+      return res.end()
     }
   console.log(req.url, req.method, req.headers);
   res.setHeader('Content-Type', 'text/html');
   res.write('<head><title>My First Page</title></head>')
-  res.write('<body><h1>Hello from my Node.js Server !</h1></body>')
+  res.write('<body><h1>Welcome to my Node Js project</h1></body>')
   res.write('</html>')
   res.end()
+  
 });
 server.listen(3000);
