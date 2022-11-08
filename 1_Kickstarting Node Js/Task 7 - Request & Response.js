b3f1931022e71ab2b1801aece9298f049005ec2e
amar8961 <amarkumar8961@gmail.com>
2022-11-08 12:59:10 +0530

    Sending Responses

--- 1_Kickstarting Node Js/Task 7 - Request & Response.js
+++ 1_Kickstarting Node Js/Task 7 - Request & Response.js
@@ -1,9 +1,23 @@
-//  *** Understanding Requests ***
+// //  *** Understanding Requests ***
+// const http = require("http");
+// const server2 = http.createServer((req, res) => {
+//   console.log(req.url, req.method, req.headers);
+//   // process.exit(); -> to quit the server
+// });
+// server2.listen(3000);
+
+
+//  *** Sending Responses ***
 const http = require("http");
-const server2 = http.createServer((req, res) => {
+const server3 = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
   // process.exit(); -> to quit the server
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<head><title>My First Page</title></head>')
+  res.write('<body><h1>Hello from my Node.js Server !</h1></body>')
+  res.write('</html>')
+  res.end()
 });
-server2.listen(3000);
+server3.listen(3000);
 
 
