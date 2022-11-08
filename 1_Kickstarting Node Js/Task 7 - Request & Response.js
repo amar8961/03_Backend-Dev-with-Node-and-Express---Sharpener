c0ce04ecfe6dd9979ccf35023ba86e86a8f2aff1
amar8961 <amarkumar8961@gmail.com>
2022-11-08 15:27:23 +0530

    Understanding Event Driven Code Execution

--- 1_Kickstarting Node Js/Task 7 - Request & Response.js
+++ 1_Kickstarting Node Js/Task 7 - Request & Response.js
@@ -77,7 +77,54 @@
 // server.listen(3000);
 
 
-//  *** Parsing Request Bodies ***
+// //  *** Parsing Request Bodies ***
+// const http = require("http");
+// const fs = require('fs');  // import 'fs' core module
+
+// const server = http.createServer((req, res) => {
+//     const url = req.url;
+//     const method = req.method;
+//     if(url === '/') {
+//         res.write('<html>')
+//         res.write('<head><title>Enter Mesage</title></head>')
+//         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>')
+//         res.write('</html>')
+//         return res.end();
+//         // return because this is not require to return a response but to return a anonymous function and to not continue with next code. it will quit the function execution.
+//     }
+//     if(url === '/message' && method === 'POST'){
+//       const body = [];
+//       req.on('data', (chunk) => {
+//         // The 'on()' method requires name of the event to handle and callback function which is called when an event is raised.
+//         console.log(chunk)
+//         body.push(chunk);
+//       });
+//       req.on('end', () => {
+//         const parsedBody = Buffer.concat(body).toString()
+//         // * The 'Buffer' class in Node. js is designed to handle raw binary data. Each buffer corresponds to some raw memory allocated outside V8. Buffers act somewhat like arrays of integers, 
+//         // but aren't resizable and have a whole bunch of methods specifically for binary data.
+//         // * The 'concat()' method concatenates the string arguments to the calling string and returns a new string.
+//         // * The 'toString()' method returns the buffer object according to the specified encoding.
+//         const message = parsedBody.split('=')[1];
+//         console.log(parsedBody)
+//         fs.writeFileSync('Task 7 - message(Parsing Request Bodies).text', message)  // it wil create a 'Task 7 - message(Redirecting Requests).text' file after submit in current directory.
+//       })
+//       res.statusCode = 302; 
+//       res.setHeader('Location', '/')
+//       return res.end(); 
+//     }
+//   console.log(req.url, req.method, req.headers);
+//   // process.exit(); -> to quit the server
+//   res.setHeader('Content-Type', 'text/html');
+//   res.write('<head><title>My First Page</title></head>')
+//   res.write('<body><h1>Hello from my Node.js Server !</h1></body>')
+//   res.write('</html>')
+//   res.end()
+// });
+// server.listen(3000);
+
+
+//  *** Understanding Event Driven Code Execution ***
 const http = require("http");
 const fs = require('fs');  // import 'fs' core module
 
@@ -90,31 +137,24 @@ const server = http.createServer((req, res) => {
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>')
         res.write('</html>')
         return res.end();
-        // return because this is not require to return a response but to return a anonymous function and to not continue with next code. it will quit the function execution.
     }
     if(url === '/message' && method === 'POST'){
       const body = [];
       req.on('data', (chunk) => {
-        // The 'on()' method requires name of the event to handle and callback function which is called when an event is raised.
         console.log(chunk)
         body.push(chunk);
       });
       req.on('end', () => {
         const parsedBody = Buffer.concat(body).toString()
-        // * The 'Buffer' class in Node. js is designed to handle raw binary data. Each buffer corresponds to some raw memory allocated outside V8. Buffers act somewhat like arrays of integers, 
-        // but aren't resizable and have a whole bunch of methods specifically for binary data.
-        // * The 'concat()' method concatenates the string arguments to the calling string and returns a new string.
-        // * The 'toString()' method returns the buffer object according to the specified encoding.
         const message = parsedBody.split('=')[1];
         console.log(parsedBody)
-        fs.writeFileSync('Task 7 - message(Parsing Request Bodies).text', message)  // it wil create a 'Task 7 - message(Redirecting Requests).text' file after submit in current directory.
+        fs.writeFileSync('Task 7 - message(Understanding Event Driven Code Execution).text', message)
+        res.statusCode = 302; 
+        res.setHeader('Location', '/')
+        return res.end();   
       })
-      res.statusCode = 302; 
-      res.setHeader('Location', '/')
-      return res.end(); 
     }
   console.log(req.url, req.method, req.headers);
-  // process.exit(); -> to quit the server
   res.setHeader('Content-Type', 'text/html');
   res.write('<head><title>My First Page</title></head>')
   res.write('<body><h1>Hello from my Node.js Server !</h1></body>')
