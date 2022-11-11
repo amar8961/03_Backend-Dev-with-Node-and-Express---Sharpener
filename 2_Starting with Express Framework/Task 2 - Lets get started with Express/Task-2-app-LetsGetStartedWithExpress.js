15e95ef1541bb1c4041c4032a453141d708a3eea
amar8961 <amarkumar8961@gmail.com>
2022-11-11 23:01:04 +0530

    *** Adding Middleware ***

--- 2_Starting with Express Framework/Task 2 - Lets get started with Express/Task-2-app-LetsGetStartedWithExpress.js
+++ 2_Starting with Express Framework/Task 2 - Lets get started with Express/Task-2-app-LetsGetStartedWithExpress.js
@@ -5,6 +5,12 @@ const express = require("express");
 
 const app = express();
 
+// use allows us to add a new middleware function.
+app.use((req, res, next) => {
+    console.log('In the middleware!')
+    next()  //  Allows the request to continue to the next middleware in line.
+})
+
 const server = http.createServer(app);
 
 server.listen(3000);
