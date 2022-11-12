00db624cd10595e496d8edb2ea3176ce84335966
amar8961 <amarkumar8961@gmail.com>
2022-11-12 23:47:58 +0530

    PROJECT- Task 5 - SaveMessageAndShowInTheHomePage

--- 2_Starting with Express Framework/Task 5 - Simple Group Chat Application - PROJECT/routes/home.js
+++ 2_Starting with Express Framework/Task 5 - Simple Group Chat Application - PROJECT/routes/home.js
@@ -2,19 +2,41 @@ const express = require("express");
 
 const router = express.Router();
 
+const fs = require("fs");
+
+// const data = require("./data");
+
 // home => GET
 router.get("/", (req, res, next) => {
   console.log("In another middleware!");
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title" placeholder="Message"><button type="submit">Send</button></form>'
-  );
+  fs.readFile("chat.text", (err, data) => {
+    if (err) {
+      console.log(err);
+      data = "No chat Exists";
+    }
+    res.send(
+      `${data}<form action="/" onsubmit="document.getElementById('username').value=localStorage.getItem('username')" method="POST">
+      <input id="message" name="message" type="text" placeholder="message">
+      <input id="username" name="username" type="hidden">
+      <button type="submit">Send</button></form>`
+    );
+  });
 });
 
-router.post("/product", (req, res, next) => {
-  // console.log(req.body);  // // req.body = [Object: null prototype] { title: 'product' }
-  const obj = JSON.parse(JSON.stringify(req.body));
-  console.log(obj); // { title: 'product' }
-  res.redirect("/");
+router.post("/", (req, res, next) => {
+  console.log(req.body.username);
+  console.log(req.body.message);
+  // { flag: "a" } -> to store old data
+  fs.writeFile(
+    "chat.text",
+    `${req.body.username} : ${req.body.message} , `,
+    { flag: "a" },
+    (err) => (err ? console.log(err) : res.redirect("/"))
+  );
+  // data.push(`{${req.body.username}:${req.body.message}}`);
+  // console.log(data);
+  // console.log(`{${req.body.username}:${req.body.message}}`);
+  // res.redirect("/");
 });
 
 module.exports = router;
