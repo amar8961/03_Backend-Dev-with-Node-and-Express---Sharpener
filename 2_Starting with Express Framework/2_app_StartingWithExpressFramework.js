391ee24b1484eb3a69792aa10708d9972e8a84d0
amar8961 <amarkumar8961@gmail.com>
2022-11-12 17:45:06 +0530

    *** Adding a 404 Error Page ***

--- 2_Starting with Express Framework/2_app_StartingWithExpressFramework.js
+++ 2_Starting with Express Framework/2_app_StartingWithExpressFramework.js
@@ -2,35 +2,6 @@ const express = require("express");
 
 const app = express();
 
-// //  *** Adding Middleware ***
-// // use allows us to add a new middleware function.
-// app.use((req, res, next) => {
-//   console.log("In the middleware!");
-//   next(); //  Allows the request to continue to the next middleware in line.
-// });
-
-// //  *** How Middleware Works ***
-// app.use((req, res, next) => {
-//   console.log("In another middleware !");
-//   res.send("<h1>Hello from Express !</h1>");
-// });
-
-// //  *** Handling Different Routes ***
-// app.use("/", (req, res, next) => {
-//   console.log("This always runs!");
-//   next();
-// });
-
-// app.use("/add-product", (req, res, next) => {
-//   console.log("In another middleware!");
-//   res.send('<h1>The "Add Product" Page</h1>');
-// });
-
-// app.use("/", (req, res, next) => {
-//   console.log("In another middleware!");
-//   res.send("<h1>Hello from Express !</h1>");
-// });
-
 //  *** Using Express Router ***
 const bodyParser = require("body-parser");
 
@@ -41,6 +12,10 @@ const ShopRoutes = require("./routes/shop");
 
 app.use(adminRoutes);
 app.use(ShopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>')  // 404 code for Page not found.
+})
 
 //  *** Express js Looking Behind the Scenes ***
 app.listen(3000);
