77fae826646c7d5c8953f48f1759211931c5f93f
amar8961 <amarkumar8961@gmail.com>
2022-11-12 11:16:26 +0530

    *** Handling Different Routes ***

--- 2_Starting with Express Framework/2_app_StartingWithExpressFramework.js
+++ 2_Starting with Express Framework/2_app_StartingWithExpressFramework.js
@@ -2,16 +2,32 @@ const express = require("express");
 
 const app = express();
 
-//  *** Adding Middleware ***
-// use allows us to add a new middleware function.
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next(); //  Allows the request to continue to the next middleware in line.
+// //  *** Adding Middleware ***
+// // use allows us to add a new middleware function.
+// app.use((req, res, next) => {
+//   console.log("In the middleware!");
+//   next(); //  Allows the request to continue to the next middleware in line.
+// });
+
+// //  *** How Middleware Works ***
+// app.use((req, res, next) => {
+//   console.log("In another middleware !");
+//   res.send("<h1>Hello from Express !</h1>");
+// });
+
+//  *** Handling Different Routes ***
+app.use("/", (req, res, next) => {
+  console.log("This always runs!");
+  next();
 });
 
-//  *** How Middleware Works ***
-app.use((req, res, next) => {
-  console.log("In another middleware !");
+app.use("/add-product", (req, res, next) => {
+  console.log("In another middleware!");
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use("/", (req, res, next) => {
+  console.log("In another middleware!");
   res.send("<h1>Hello from Express !</h1>");
 });
 
