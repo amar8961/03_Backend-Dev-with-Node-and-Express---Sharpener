7d8b8460066a8b955b638c4d2846e880e56494bf
amar8961 <amarkumar8961@gmail.com>
2022-11-12 11:53:20 +0530

    install body parser -> Parsing Incoming Requests

--- 2_Starting with Express Framework/2_app_StartingWithExpressFramework.js
+++ 2_Starting with Express Framework/2_app_StartingWithExpressFramework.js
@@ -15,15 +15,39 @@ const app = express();
 //   res.send("<h1>Hello from Express !</h1>");
 // });
 
-//  *** Handling Different Routes ***
-app.use("/", (req, res, next) => {
-  console.log("This always runs!");
-  next();
-});
+// //  *** Handling Different Routes ***
+// app.use("/", (req, res, next) => {
+//   console.log("This always runs!");
+//   next();
+// });
+
+// app.use("/add-product", (req, res, next) => {
+//   console.log("In another middleware!");
+//   res.send('<h1>The "Add Product" Page</h1>');
+// });
+
+// app.use("/", (req, res, next) => {
+//   console.log("In another middleware!");
+//   res.send("<h1>Hello from Express !</h1>");
+// });
+
+//  *** Parsing Incoming Requests ***
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false })); // with this we get product details in TERMINAL as Key : Value
 
 app.use("/add-product", (req, res, next) => {
   console.log("In another middleware!");
-  res.send('<h1>The "Add Product" Page</h1>');
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form?'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  // console.log(req.body);  // // req.body = [Object: null prototype] { title: 'product' }
+  const obj = JSON.parse(JSON.stringify(req.body));
+  console.log(obj); // { title: 'product' }
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
