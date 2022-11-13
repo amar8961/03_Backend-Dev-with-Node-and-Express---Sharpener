86f81b3059e51c0b769af9f5f91e18c26b3c6a60
amar8961 <amarkumar8961@gmail.com>
2022-11-13 22:25:18 +0530

    *** Serving HTML Pages ***

--- 2_Starting with Express Framework/Task 6 - Serving HTML, CSS and JS (VIEWS)/routes/admin.js
+++ 2_Starting with Express Framework/Task 6 - Serving HTML, CSS and JS (VIEWS)/routes/admin.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
@@ -5,12 +7,10 @@ const router = express.Router();
 // admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   console.log("In another middleware!");
-  res.send(
-    '<form action="/admin/product" method="POST"><input type="text" name="title" placeholder="Product"><input type="text" name="title" placeholder="Size"><button type="submit">Add Product</button></form>'
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
-// admin/add-product => POST 
+// admin/add-product => POST
 // we reach admin/product because 'admin.js' all files request via '2_app_StartingWithExpressFramework.js' app.use('/admin', adminRoutes);
 router.post("/product", (req, res, next) => {
   // console.log(req.body);  // // req.body = [Object: null prototype] { title: 'product' }
