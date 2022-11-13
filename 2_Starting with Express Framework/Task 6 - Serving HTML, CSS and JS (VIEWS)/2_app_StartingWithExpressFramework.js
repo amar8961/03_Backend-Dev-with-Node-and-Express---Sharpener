9249a3274c281a96165dadd8c2f729fa29042205
amar8961 <amarkumar8961@gmail.com>
2022-11-13 22:39:08 +0530

    *** Returning a 404 Page ***

--- 2_Starting with Express Framework/Task 6 - Serving HTML, CSS and JS (VIEWS)/2_app_StartingWithExpressFramework.js
+++ 2_Starting with Express Framework/Task 6 - Serving HTML, CSS and JS (VIEWS)/2_app_StartingWithExpressFramework.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const path = require("path");
+
 const app = express();
 
 //  *** Using Express Router ***
@@ -8,15 +10,15 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false })); // with this we get product details in TERMINAL as Key : Value
 
 const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");  // create shop route
+const shopRoutes = require("./routes/shop"); // create shop route
 
-app.use('/admin', adminRoutes);  // *** Filtering Paths ***
+app.use("/admin", adminRoutes); // *** Filtering Paths ***
 app.use(shopRoutes); // use shop route
 
 // *** Adding a 404 Error Page ***
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>')  // 404 code for Page not found.
-})
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html')); // 404 code for Page not found.
+});
 
 //  *** Express js Looking Behind the Scenes ***
 app.listen(3000);
