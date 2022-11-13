2d0365a58bce9ec2992755f7733a011d6d5b9c7f
amar8961 <amarkumar8961@gmail.com>
2022-11-14 02:23:57 +0530

    PROJECT - Task - 6 -> add contact us button

--- 2_Starting with Express Framework/Task 6 - Serving HTML, CSS and JS (VIEWS)/2_app_StartingWithExpressFramework.js
+++ 2_Starting with Express Framework/Task 6 - Serving HTML, CSS and JS (VIEWS)/2_app_StartingWithExpressFramework.js
@@ -8,13 +8,17 @@ const app = express();
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false })); // with this we get product details in TERMINAL as Key : Value
-app.use(express.static(path.join(__dirname, "public")));  // with this users should be able to access 'public' path // it will take any request that tries to find some file.
+app.use(express.static(path.join(__dirname, "public"))); // with this users should be able to access 'public' path // it will take any request that tries to find some file.
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop"); // create shop route
+const contactUsRoutes = require("./routes/contactus"); // create contact us route
+const successRoutes = require("./routes/success"); // create success route
 
 app.use("/admin", adminRoutes); // *** Filtering Paths ***
 app.use(shopRoutes); // use shop route
+app.use(contactUsRoutes); // use contact us route
+app.use(successRoutes); // use success route
 
 // *** Adding a 404 Error Page ***
 app.use((req, res, next) => {
