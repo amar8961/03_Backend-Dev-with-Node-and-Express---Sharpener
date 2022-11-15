a18a439564ce39cd49835ce6683682204893d7aa
amar8961 <amarkumar8961@gmail.com>
2022-11-15 14:45:31 +0530

    *** 126. Linking to the Edit Page ***

--- 2_Starting with Express Framework/Task 11 - Dynamic Routes Part 3 (Query Params)/Continue with Task - 10 Files/controllers/admin.js
+++ 2_Starting with Express Framework/Task 11 - Dynamic Routes Part 3 (Query Params)/Continue with Task - 10 Files/controllers/admin.js
@@ -37,6 +37,10 @@ exports.getEditProduct = (req, res, next) => {
   });
 };
 
+exports.postEditProduct = (req, res, nest) => {
+  //
+}
+
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("admin/products", {
