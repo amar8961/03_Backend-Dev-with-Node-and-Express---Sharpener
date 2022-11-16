8776abdc57dc6561c9e4f7f041c79c7f8922c6be
amar8961 <amarkumar8961@gmail.com>
2022-11-16 14:40:43 +0530

    *** 144. Fetching Products - Time to Practice ***

--- 2_Starting with Express Framework/Task 16 - Retrieving data from databases/Continue with Task - 15 Files/controllers/shop.js
+++ 2_Starting with Express Framework/Task 16 - Retrieving data from databases/Continue with Task - 15 Files/controllers/shop.js
@@ -2,13 +2,15 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/product-list", {
-      prods: products,
-      pageTitle: "All Products",
-      path: "/products",
-    });
-  });
+  Product.fetchAll()
+    .then(([rown, fieldData]) => {
+      res.render("shop/product-list", {
+        prods: rown,
+        pageTitle: "All Products",
+        path: "/products",
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getProduct = (req, res, next) => {
