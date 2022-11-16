c12771164acfa1ca9ec8d103acefc44788be3a7e
amar8961 <amarkumar8961@gmail.com>
2022-11-16 19:05:34 +0530

    *** 151. Connecting to the Database ***

--- 2_Starting with Express Framework/Task 17 - Making SQL easy with Sequelize/Continue with Task - 16 Files/util/database.js
+++ 2_Starting with Express Framework/Task 17 - Making SQL easy with Sequelize/Continue with Task - 16 Files/util/database.js
@@ -1,10 +1,20 @@
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
 
-const pool = mysql.createPool({
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "amar",
+//   password: "123456",
+// });
+
+// module.exports = pool.promise();
+
+const Sequelize = require("sequelize");
+
+// SCHEMAS Name = amar , User Name = root , Password = 123456
+const sequelize = new Sequelize("amar", "root", "123456", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "amar",
-  password: "123456",
 });
 
-module.exports = pool.promise();
+module.exports = Sequelize;
