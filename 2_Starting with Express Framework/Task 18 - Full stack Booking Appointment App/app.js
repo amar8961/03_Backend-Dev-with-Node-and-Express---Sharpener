521c5943e075828018a66e71c7f7cb64f0ce0c6e
amar8961 <amarkumar8961@gmail.com>
2022-11-17 18:10:49 +0530

    Task 18 -> Full stack Booking Appointment App

---
+++ 2_Starting with Express Framework/Task 18 - Full stack Booking Appointment App/app.js
@@ -0,0 +1,9 @@
+// Express and nodemon installed
+const express = require("express");
+
+const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(4000);
