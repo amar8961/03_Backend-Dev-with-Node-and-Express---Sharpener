2be446637ce65d555a09c5b7d6c4e9c2db974d5b
amar8961 <amarkumar8961@gmail.com>
2022-11-18 18:36:35 +0530

    Public & CSS -> Task 19 - Full stack Expense App

--- 2_Starting with Express Framework/Task 19 - Full stack Expense App/app.js
+++ 2_Starting with Express Framework/Task 19 - Full stack Expense App/app.js
@@ -7,8 +7,12 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json({ extended: false }));
 
 // route for home page
-const homeRoutes = require('./routes/homeroutes')
-app.use(homeRoutes)
+const homeRoutes = require("./routes/homeroutes");
+app.use(homeRoutes);
+
+// with this users should be able to access 'public' path // it will take any request that tries to find some file.
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
 
 // to bind and listen the connections on the specified host and port.
 app.listen(5000);
