999c29bccef9ccc7b507176883bb581881346725
amar8961 <amarkumar8961@gmail.com>
2022-11-18 19:50:29 +0530

    Save Data -> Task 19 - Full stack Expense App

--- 2_Starting with Express Framework/Task 19 - Full stack Expense App/controllers/homecontrollers.js
+++ 2_Starting with Express Framework/Task 19 - Full stack Expense App/controllers/homecontrollers.js
@@ -10,3 +10,29 @@ exports.getHome = (req, res, next) => {
 
 // Import models
 const Expenses = require("../models/homemodels");
+
+// create controller for save data
+exports.addData = async (req, res, next) => {
+  try {
+    if (!req.body.category) {
+      throw new Error("category is mandatory");
+    }
+    const expenses = req.body.expenses;
+    const description = req.body.description;
+    const category = req.body.category;
+    const data = await Expenses.create({
+      expenses: expenses,
+      description: description,
+      category: category,
+    });
+    // newExpenses -> send data to frontend
+    res.status(201).json({ newExpenses: data }); // The HTTP 201 Created success status response code
+    // res.sendFile(path.join(rootDir, "views", "home.html"));
+  } catch (err) {
+    console.log("ERROR");
+    // HTTP 500 Internal Server Error server error response code.
+    res.status(500).json({
+      error: err,
+    });
+  }
+};
--- 2_Starting with Express Framework/Task 19 - Full stack Expense App/routes/homeroutes.js
+++ 2_Starting with Express Framework/Task 19 - Full stack Expense App/routes/homeroutes.js
@@ -5,7 +5,10 @@ const router = express.Router();
 // import home controllers
 const homeController = require("../controllers/homecontrollers");
 
-// create route for home ('/) page.
+// create route for home ('/') page.
 router.get("/", homeController.getHome);
+
+// create route for add data.
+router.post('/add', homeController.addData)
 
 module.exports = router;
