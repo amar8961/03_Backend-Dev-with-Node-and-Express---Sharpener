4f65029c2168f5cb79bd7f521d5fc21e328402ec
amar8961 <amarkumar8961@gmail.com>
2022-11-18 11:04:42 +0530

    2nd UPDATE->Save Data in Database->Appointment App

--- 2_Starting with Express Framework/Task 18 - Full stack Booking Appointment App/app.js
+++ 2_Starting with Express Framework/Task 18 - Full stack Booking Appointment App/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 // It provides four express middleware for parsing JSON, Text, URL-encoded, and raw data sets over an HTTP request body.
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ extended: false }));
 
 const homeRoutes = require("./routes/homeroutes");
 app.use(homeRoutes);
