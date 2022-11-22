545b731b7a5de6b02439e198d4603c0497d8680b
amar8961 <amarkumar8961@gmail.com>
2022-11-22 16:44:31 +0530

    Changed the cart layout -> Task 10

--- 3_Building an Ecommerce website/Task 10 - Changing the cart layout/script.js
+++ 3_Building an Ecommerce website/Task 10 - Changing the cart layout/script.js
@@ -1,121 +1,75 @@
-if (document.readyState == 'loading') {
-    // The Document.readyState property describes the loading state of the document. When the value of this property changes, a readystatechange event fires on the document object.
-    // loading -> The document is still loading.
-    document.addEventListener('DOMContentLoaded', ready)
-    // The DOMContentLoaded event fires when the DOM content is loaded, without waiting for images and stylesheets to finish loading.
-} else {
-    ready()
-}
+const cart_items = document.querySelector("#cart .cart-items");
 
-function ready() {
-    var removeCartItemButtons = document.getElementsByClassName('btn-remove')
-    for (var i = 0; i < removeCartItemButtons.length; i++) {
-        var button = removeCartItemButtons[i]
-        button.addEventListener('click', removeCartItem)
-        // The addEventListener() method attaches an event handler to an element.
+const parentContainer = document.getElementById("EcommerceContainer");
+parentContainer.addEventListener("click", (e) => {
+    // DOM element with className == "shop-item-button"
+  if (e.target.className == "shop-item-button") {
+    const id = e.target.parentNode.parentNode.id;
+    const name = document.querySelector(`#${id} h3`).innerText;
+    const img_src = document.querySelector(`#${id} img`).src;
+    const price = e.target.parentNode.firstElementChild.firstElementChild.innerText;
+      
+    let total_cart_price = document.querySelector("#total-value").innerText;  // id -> #total-value
+    // if item is already in your cart show a alert "This item is already added to the cart"
+    if (document.querySelector(`#in-cart-${id}`)) {
+      alert("This item is already added to the cart");
+      return;
     }
 
-    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
-    for (var i = 0; i < quantityInputs.length; i++) {
-        var input = quantityInputs[i]
-        input.addEventListener('change', quantityChanged)
-    }
+    document.querySelector(".cart-number").innerText = parseInt(document.querySelector(".cart-number").innerText) + 1;  // class='cart-number'
+    const cart_item = document.createElement("div");
+    cart_item.classList.add("cart-row");
+    cart_item.setAttribute("id", `in-cart-${id}`);
+    total_cart_price = parseFloat(total_cart_price) + parseFloat(price);
+    total_cart_price = total_cart_price.toFixed(2);
+    document.querySelector("#total-value").innerText = `${total_cart_price}`;
+    cart_item.innerHTML = `
+        <span class='cart-item cart-column'>
+        <img class='cart-img' src="${img_src}" alt="">
+            <span>${name}</span>
+    </span>
+    <span class='cart-price cart-column'>${price}</span>
+    <span class='cart-quantity cart-column'>
+        <input type="text" value="1">
+        <button>REMOVE</button>
+    </span>`;
+    cart_items.appendChild(cart_item);
 
-    var addToCartButtons = document.getElementsByClassName('shop-item-button')
-    for (var i = 0; i < addToCartButtons.length; i++) {
-        var button = addToCartButtons[i]
-        button.addEventListener('click', addToCartClicked)
-    }
-
-    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
-}
-
-function purchaseClicked() {
-    alert('Thank you for your purchase')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    while (cartItems.hasChildNodes()) {
-        cartItems.removeChild(cartItems.firstChild)
-    }
-    updateCartTotal()
-}
-
-function removeCartItem(event) {
-    var buttonClicked = event.target
-    buttonClicked.parentElement.parentElement.remove()
-    updateCartTotal()
-}
-
-function quantityChanged(event) {
-    var input = event.target
-    if (isNaN(input.value) || input.value <= 0) {
-        input.value = 1
-    }
-    updateCartTotal()
-}
-
-function addToCartClicked(event) {
-    var button = event.target
-    var shopItem = button.parentElement.parentElement
-    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
-    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
-    var imageSrc = shopItem.getElementsByClassName('prod-images')[0].src
-    addItemToCart(title, price, imageSrc)
-    updateCartTotal()
-}
-
-function addItemToCart(title, price, imageSrc) {
-    var cartRow = document.createElement('div')
-    cartRow.classList.add('cart-row')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
-    for (var i = 0; i < cartItemNames.length; i++) {
-        if (cartItemNames[i].innerText == title) {
-            alert('This item is already added to the cart')
-            return
-        }
-    }
-    var cartRowContents = `
-        <div class="cart-item cart-column">
-            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
-            <span class="cart-item-title">${title}</span>
-        </div>
-        <span class="cart-price cart-column">${price}</span>
-        <div class="cart-quantity cart-column">
-            <input class="cart-quantity-input" type="number" value="1">
-            <button class="btn btn-remove" type="button">REMOVE</button>
-        </div>`
-    cartRow.innerHTML = cartRowContents
-    cartItems.append(cartRow)
-    // The append() method appends an element to the end of the list.
-    cartRow.getElementsByClassName('btn-remove')[0].addEventListener('click', removeCartItem)
-    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
-
-    const container = document.getElementById('notification-container');
-    const notification = document.createElement('div');
-    notification.classList.add('notification');
-    // The classList property returns the CSS classnames of an element.
-    notification.innerHTML = `<h4>Your Product : <span>${title}</span> is added to the cart<h4>`;
+    const container = document.getElementById("notification-container");
+    const notification = document.createElement("div");
+    notification.classList.add("notification");
+    notification.innerHTML = `<h4>Your Product : <span>${name}</span> is added to the cart<h4>`;
     container.appendChild(notification);
-    // The appendChild() method of the node interface, is used to create a text node as the last child of the node. This method is also used to move an element from one element to another element.
-    setTimeout(()=>{
-        notification.remove();
-    },2500)
-}
-
-function updateCartTotal() {
-    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
-    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
-    var total = 0
-    for (var i = 0; i < cartRows.length; i++) {
-        var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-        var price = parseFloat(priceElement.innerText.replace('₹', ''))
-        // The parseFloat function converts its first argument to a string, parses that string as a decimal number literal, then returns a number or NaN .
-        var quantity = quantityElement.value
-        total = total + (price * quantity)
+    setTimeout(() => {
+      notification.remove();
+    }, 2500);
+  }
+  if (
+    e.target.className == "cart-btn-bottom" ||
+    e.target.className == "cart-bottom" ||
+    e.target.className == "cart-holder"
+  ) {
+    document.querySelector("#cart").style = "display:block;";
+  }
+  if (e.target.className == "cancel") {
+    document.querySelector("#cart").style = "display:none;";
+  }
+  if (e.target.className == "purchase-btn") {
+    if (parseInt(document.querySelector(".cart-number").innerText) === 0) {
+      alert("You have Nothing in Cart , Add some products to purchase !");
+      return;
     }
-    total = Math.round(total * 100) / 100
-    // The Math.round() function returns the value of a number rounded to the nearest integer.
-    document.getElementsByClassName('cart-total-price')[0].innerText = '₹' + total
-}
+    alert("Thanks for the purchase");
+    cart_items.innerHTML = "";
+    document.querySelector(".cart-number").innerText = 0;
+    document.querySelector("#total-value").innerText = `0`;
+  }
+
+  if (e.target.innerText == "REMOVE") {
+    let total_cart_price = document.querySelector("#total-value").innerText;
+    total_cart_price = parseFloat(total_cart_price).toFixed(2) - parseFloat(document.querySelector(`#${e.target.parentNode.parentNode.id} .cart-price`).innerText).toFixed(2);
+    document.querySelector(".cart-number").innerText = parseInt(document.querySelector(".cart-number").innerText) - 1;
+    document.querySelector("#total-value").innerText = `${total_cart_price.toFixed(2)}`;
+    e.target.parentNode.parentNode.remove();
+  }
+});
