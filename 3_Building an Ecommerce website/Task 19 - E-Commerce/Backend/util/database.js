44a6418cd3ffb98166f07e638a830e2fbb00a810
amar8961 <113022561+amar8961@users.noreply.github.com>
2022-11-27 18:26:07 +0530

    User and Product Relation

--- 3_Building an Ecommerce website/Task 19 - E-Commerce/Backend/util/database.js
+++ 3_Building an Ecommerce website/Task 19 - E-Commerce/Backend/util/database.js
@@ -1,20 +1,8 @@
-// const mysql = require("mysql2");
+const Sequelize = require('sequelize');
 
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   database: "amar",
-//   password: "123456",
-// });
-
-// module.exports = pool.promise();
-
-const Sequelize = require("sequelize");
-
-// SCHEMAS Name = amar , User Name = root , Password = 123456
-const sequelize = new Sequelize("amar", "root", "123456", {
-  dialect: "mysql",
-  host: "localhost",
+const sequelize = new Sequelize('amar', 'root', '123456', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
 module.exports = sequelize;
