7cca3f1e1322bb00b71f2330257d527f5c9c2407
amar8961 <113022561+amar8961@users.noreply.github.com>
2022-12-10 14:31:38 +0530

    Sign in

--- 4.1_Expense Tracker/Frontend/auth.js
+++ 4.1_Expense Tracker/Frontend/auth.js
@@ -14,21 +14,31 @@ signInButton.addEventListener('click', () => {
 // Start
 let UserUrl="http://localhost:4000/users"
 let signUpBtn=document.getElementById("sign-up-btn")
+let signInBtn=document.getElementById("sign-in-btn")
 
 signUpBtn.addEventListener('click', signUp)
+signInBtn.addEventListener('click', signIn)
 
+// Sign up
 function signUp(){
     const name=document.getElementById('name-up').value
     const email=document.getElementById('email-up').value
     const password=document.getElementById('pass-up').value
     if (name.length<3 || !isNaN(name) || name==" "){
         alert("Enter a valid name!")
+        return
     }
     else if (email.length<5 || email.indexOf('@')==-1){
         alert("Enter a valid email!")
+        return
     }
     else if(password.length<4){
         alert("Enter a strong password!")
+        return
+    }else{
+        document.getElementById('name-up').value=""
+        document.getElementById('email-up').value=""
+        document.getElementById('pass-up').value=""
     }
     axios({
         method: 'post',
@@ -50,3 +60,32 @@ function signUp(){
     }).catch(err=>console.log(err))
 }
 
+// Sign in
+signInBtn.addEventListener('click', signIn)
+
+function signIn(){
+    const email=document.getElementById('email-in').value
+    const password=document.getElementById('pass-in').value
+    if (email.length<5 || email.indexOf('@')==-1){
+        alert("Enter a valid email!")
+        return
+    }
+    else{
+        document.getElementById('email-in').value=""
+        document.getElementById('pass-in').value=""
+    }
+    axios({
+        method: 'get',
+        url: `${UserUrl}/${email}`,
+    }).then(response=>{
+        console.log(response)
+        if (response.data==""){
+            alert("Your email is not registered with us!")
+        }else if(response.data.password!==password){
+            alert("You've entered an incorrect password!")
+        }else{
+            alert("Sign In Successful!")
+            location.replace('./index.html')
+        }
+    }).catch(err=>console.log(err))
+}
